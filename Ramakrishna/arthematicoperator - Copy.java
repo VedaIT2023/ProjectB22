c3c226589b8fa62902a286dd02963f371ec187b9
roddannagarikavitha <kavitharangaswamy08@gmail.com>
2023-09-13 09:31:09 +0530

    Ramakrishna folder added

---
+++ Ramakrishna/arthematicoperator - Copy.java
@@ -0,0 +1,18 @@
+public class Datatypes{
+public static void main(String[] args){
+int a,b,c,sum,sub,mul;
+float div,mod;
+ a=30;
+ b=20;
+sum = a+b;
+sub  =  a-b;
+mul = a*b;
+ div = (float)a/b;
+ mod =(float)a%b;
+System.out.println("sum is c"+ sum);
+System.out.println("sub is c"+ sub);
+System.out.println("mul is c"+ mul);
+System.out.println("div is c"+ div);
+System.out.println("mod is c"+ mod);
+ }
+}
