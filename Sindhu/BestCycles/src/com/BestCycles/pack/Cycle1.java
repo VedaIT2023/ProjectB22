f6090891c68b2d690dfb49a2c028a875bca506af
Sindhu1234 <sindhu.satya1601@gmail.com>
2023-09-14 13:27:20 +0530

    sindhu

---
+++ Sindhu/BestCycles/src/com/BestCycles/pack/Cycle1.java
@@ -0,0 +1,25 @@
+package com.BestCycles.pack;
+
+public class Cycle1 {
+	public static void printLadyBirdCycleFeatures() {
+		
+		String name = "LadyBird";
+		int brakes = 2;
+		int handle = 1;
+		int pedal = 2;
+		char type = 'F';
+		int tyres = 2;
+		int gears = 6;
+		int price = 6000;
+		
+		System.out.println("Name of the cycle " +  name);
+		System.out.println("Type of the cycle " +  type);
+		System.out.println("No of tyres = " + tyres);
+		System.out.println("No of handle = " + handle);
+		System.out.println("No of pedal = " + pedal);
+		System.out.println("No of brakes = " + brakes);
+		System.out.println("No of Gears = " + gears);
+		System.out.println("Price  " + price);
+		
+	}
+}
---
+++ Sindhu/BestCycles/src/com/BestCycles/pack/Cycles.java
@@ -0,0 +1,19 @@
+package com.BestCycles.pack;
+import java.util.*;
+
+
+public class Cycles {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		 System.out.println("BSA Cycle Features:");
+		 Cycle.printBSACycleFeatures();
+		 System.out.println("\n\n");
+		 System.out.println("LadyBird Cycle Features:");
+		 Cycle1.printLadyBirdCycleFeatures();
+
+	}
+}
