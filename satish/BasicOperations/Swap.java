467f9f4aeb72516251ac0d938652fd3d9b57ce59
satishpendhem <satishpendhem052@gmail.com>
2023-09-14 09:32:27 +0530

    satish files added

---
+++ satish/BasicOperations/Swap.java
@@ -0,0 +1,11 @@
+public class swap {
+	public static void main(String[] args){
+	int a = 2, b =4;
+	int temp = 0;
+	System.out.println("Intially Numbers are:" +a+" "+b);
+	temp = a;
+	a = b;
+	b = temp;
+	System.out.println("After Swapping Two Numbers is:" +a+" "+b);
+	}
+}
