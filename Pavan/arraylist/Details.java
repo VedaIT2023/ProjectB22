40d87d3229371e8a6bd51baf00c8dcfcae8d764a
Pavan Cherry <pavancherry1239@gmail.com>
2023-09-15 07:26:30 +0530

    Arraylist  file added

---
+++ Pavan/arraylist/Details.java
@@ -0,0 +1,13 @@
+package com.vedait.studentdetails.arraylist;
+
+import java.util.ArrayList;
+
+public class Details {
+
+	ArrayList<Students>stu;
+	
+	Details(ArrayList<Students>stu){
+	 this.stu = stu;
+	
+	}
+}
---
+++ Pavan/arraylist/Students.java
@@ -0,0 +1,19 @@
+package com.vedait.studentdetails.arraylist;
+
+public class Students {
+	
+	String name,email,phno;
+	int age,idno;
+	char gender;
+	
+	Students (String name,String email,String phno,int age, char gender, int idno){
+		
+		this.name = name;
+		this.email = email;
+		this.phno = phno;
+		this.age = age;
+		this.gender =  gender;
+		this.idno = idno;
+	}
+
+}
