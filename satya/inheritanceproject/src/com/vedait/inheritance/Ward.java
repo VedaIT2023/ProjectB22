40e2412d58081d7a84280db4cf33dba08e5dabd6
Satyasri9141 <144755969+Satyasri9141@users.noreply.github.com>
2023-09-20 12:05:11 +0530

    Previous tasks

---
+++ satya/inheritanceproject/src/com/vedait/inheritance/Ward.java
@@ -0,0 +1,18 @@
+package com.vedait.inheritance;
+
+public class Ward {
+	String wardName;
+    int wardNumber;
+    int capacity;
+    
+	Ward(String wardName,int wardNumber,int capacity) {
+		
+		// TODO Auto-generated constructor stub
+		 this.wardName=wardName;
+		 this.wardNumber=wardNumber;
+		 this.capacity=capacity;
+	}
+	
+}
+          
+
