41ef660b24fba5442d0bf47d1f37bf7e9ede79bb
UserSindhu1234 <142030856+UserSindhu1234@users.noreply.github.com>
2023-09-27 07:42:08 +0530

    sins

---
+++ StudentTasks/Sindhu/Androids/src/com/Different/Androids/Phones.java
@@ -0,0 +1,12 @@
+package com.Different.Androids;
+
+public class Phones {
+	static void samsungPhone() {
+    
+    	System.out.println("SamsungPhoneFeatures : ");
+	}
+	static void OnePlusPhone() {
+		System.out.println("OnePlusPhoneFeatures : ");
+	}
+
+}
