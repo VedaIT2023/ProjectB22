2f106556edf7292ba152d8c2db3d3e3243fef7ac
fsdbatchjuly <fsdbatchjuly2023@gmail.com>
2023-09-30 20:50:29 +0530

    Code flow

---
+++ DailyClass/Sep29_A2ZHosp/src/HospitalHelper.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+
+public class HospitalHelper {
+	
+	static public ArrayList<Doctor> doctorsList = new ArrayList(); 
+	
+	static void createDoctors() {
+						
+		
+		
+		addDoctor("Dinesh", "Cardio");
+		addDoctor("Satish", "Physio");
+		addDoctor("Sindhu", "Physio");
+		addDoctor("Satya", "Physio");
+		addDoctor("KAvita", "Physio");
+		addDoctor("Balaji", "Physio");
+		addDoctor("Kalyan", "Physio");
+		addDoctor("Valli", "Physio");
+		addDoctor("Naren", "Physio");
+	}
+	
+	static void displayDoctorsList() {
+		for(int i=0; i<doctorsList.size();i++) {
+			Doctor d = HospitalHelper.doctorsList.get(i);
+			System.out.println(d.id + " " + d.name + " " + d.specialisation);
+		}
+
+	}
+	
+	
+	private static void addDoctor(String name, String specialisation) {			
+		
+		int id = 0;
+		if(doctorsList.size()>0) {
+			id = doctorsList.size()+1;
+		}else {
+			id = 1;
+		}
+		Doctor doc = new Doctor(name,id,specialisation,"","");
+		doctorsList.add(doc);
+	}
+		
+
+}
