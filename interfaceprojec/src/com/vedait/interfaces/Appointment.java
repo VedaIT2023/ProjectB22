c73db2abac0a0f25dc9d4896c238d75c8e506163
roddangarikavitha <144756122+roddangarikavitha@users.noreply.github.com>
2023-10-02 21:42:56 +0530

    Hospital task added

---
+++ interfaceprojec/src/com/vedait/interfaces/Appointment.java
@@ -0,0 +1,12 @@
+package com.vedait.interfaces;
+
+public class Appointment {
+	String patientName;
+	int drId;
+	int patientId;
+	
+	 void Appoinment(String patientName,int drId,int patientId){
+		this.patientName=patientName;
+		this.patientId=patientId;
+	}
+}
---
+++ interfaceprojec/src/com/vedait/interfaces/Patient.java
@@ -0,0 +1,18 @@
+package com.vedait.interfaces;
+
+public class Patient {
+	
+	String name;
+	String typeProblem;
+	String phoneNumber;
+	String timings;
+	
+	Patient(String name,String typeProblem,String phoneNumber,String timings){
+		this.name=name;
+		this.typeProblem=typeProblem;
+		this.phoneNumber=phoneNumber;
+		this.timings=timings;
+	}
+	
+
+}
