9596003bae95632c3ec19e8dec889061031a492d
Satyasri9141 <144755969+Satyasri9141@users.noreply.github.com>
2023-10-04 07:33:06 +0530

    ...

--- satish/BasicOperations/StringName.java
+++
@@ -1,9 +0,0 @@
-public class StringName {
-	public static void main(String[] args){
-	String[] Name = {"Satish","Balaji","Lucky","Sathwik","Pavan"};
-	System.out.println("Length of the Names is:" + Name.length);
-	for (int i=0;i<Name.length;i++) {
-		System.out.println(Name[i]);
-		}
-	}
-}
