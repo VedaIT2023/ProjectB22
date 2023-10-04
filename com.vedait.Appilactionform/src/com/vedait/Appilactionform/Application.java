2190541ee369ffc7862f87f4cfe2274ba6b216e6
sunandasriravuri <sunandasriravuri63@gmail.com>
2023-10-04 07:30:05 +0530

    Sunanda adding a file

---
+++ com.vedait.Appilactionform/src/com/vedait/Appilactionform/Application.java
@@ -0,0 +1,162 @@
+package com.vedait.Appilactionform;
+import java.util.Scanner;
+import java.util.*;
+import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Application {
+    static HashMap<Integer, ArrayList<String>> appointments = new HashMap<>();
+    static Integer appointmentCounter = 1000;
+
+    public static void main(String[] args) {
+        Doctors.doctorDetails();
+        showOptions();
+    }
+
+    static Integer getAppointmentId() {
+        appointmentCounter++;
+        return appointmentCounter;
+    }
+
+    static void bookAppointment(String doctorId) {
+        Scanner sc = new Scanner(System.in);
+        boolean found = false;
+        ArrayList<Doctors> doctorsList = Doctors.doctorsList;
+
+        for (Doctors doctor : doctorsList) {
+            if (doctor.getId().equals(doctorId)) {
+                doctor.displayDoctorInfo();
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Doctor Not Found");
+            return;
+        }
+
+        System.out.println("Do You Want To Book an Appointment (Yes/No)");
+        String input = sc.next();
+
+        if (input.equalsIgnoreCase("yes")) {
+            System.out.println("Enter your Name");
+            String name = sc.next();
+            System.out.println("Enter your Problem");
+            String problem = sc.next();
+            System.out.println("Enter your phone no.");
+            String phone = sc.next();
+
+          
+            System.out.println("Available Appointment Slots:");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+         
+            Date slot1 = new Date();
+            Date slot2 = new Date();
+            Date slot3 = new Date();
+            Date slot4 = new Date();
+            Date slot5 = new Date();
+            Date slot6 = new Date();
+            Date slot7 = new Date();
+            Date slot8 = new Date();
+
+            System.out.println("1. " + dateFormat.format(slot1));
+            System.out.println("2. " + dateFormat.format(slot2));
+            System.out.println("3. " + dateFormat.format(slot3));
+            System.out.println("4. " + dateFormat.format(slot4));
+            System.out.println("5. " + dateFormat.format(slot5));
+            System.out.println("6. " + dateFormat.format(slot6));
+            System.out.println("7. " + dateFormat.format(slot7));
+            System.out.println("8. " + dateFormat.format(slot8));
+            System.out.println("Select an appointment slot (1, 2,3,4,5,6,7,8):");
+            int slotChoice = sc.nextInt();
+
+            Date selectedSlot = null;
+
+            switch (slotChoice) {
+                case 1:
+                    selectedSlot = slot1;
+                    break;
+                case 2:
+                    selectedSlot = slot2;
+                    break;
+                case 3:
+                    selectedSlot = slot3;
+                    break;
+                case 4:
+                    selectedSlot = slot4;
+                    break;
+                case 5:
+                    selectedSlot = slot5;
+                    break;
+                case 6:
+                    selectedSlot = slot6;
+                    break;
+                case 7:
+                    selectedSlot = slot7;
+                    break;
+                case 8:
+                    selectedSlot = slot8;
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+                    return;
+            }
+
+           
+            ArrayList<String> appointmentDetails = new ArrayList<>();
+            appointmentDetails.add(doctorId);
+            appointmentDetails.add(name);
+            appointmentDetails.add(problem);
+            appointmentDetails.add(phone);
+            appointmentDetails.add(dateFormat.format(selectedSlot));
+
+            Integer appointmentID = getAppointmentId();
+
+            appointments.put(appointmentID, appointmentDetails);
+
+            System.out.println("Your appointment is booked with Appointment ID: " + appointmentID);
+
+        } else if (input.equalsIgnoreCase("no")) {
+            System.out.println("Please select another doctor.");
+        } else {
+            System.out.println("Invalid input. Please enter 'yes' or 'no'.");
+        }
+    }
+
+    public static void showOptions() {
+        Scanner sc = new Scanner(System.in);
+        int options;
+
+        do {
+          
+            System.out.println("Select any Option");
+            System.out.println("1. Book an Appointment");
+            System.out.println("2. View Doctors Information");
+            System.out.println("3. View Appointments Information");
+            System.out.println("4. Delete an Appointment");
+            System.out.println("5. Exit");
+            options = sc.nextInt();
+
+            switch (options) {
+                case 1:
+                    System.out.println("Enter the Doctor ID");
+                    String doctorId = sc.next();
+                    bookAppointment(doctorId);
+                    break;
+                case 2:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid Option. Please enter a valid option.");
+                    break;
+            }
+        } while (options != 2);
+    }
+}
+
+
+
+
