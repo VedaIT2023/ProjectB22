3b53b6f944dcf6f260b24453bd4b61e36ff2e9ae
sathwik028 <143501762+sathwik028@users.noreply.github.com>
2023-10-04 07:32:47 +0530

    codepush

---
+++ arraylist/Doctor.java
@@ -0,0 +1,50 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class Doctor {
+    String name, specialisation, timings, id, age;
+
+    static ArrayList<Doctor> doctorList;
+
+    Doctor(String name, String specialisation, String timings, String id, String age) {
+        this.name = name;
+        this.specialisation = specialisation;
+        this.timings = timings;
+        this.id = id;
+        this.age = age;
+    }
+
+    public void displayDoctorInfo() {
+        System.out.println("Name of the Doctor: " + name);
+        System.out.println("ID No: " + id);
+        System.out.println("Specialisation: " + specialisation);
+        System.out.println("Timings: " + timings);
+        System.out.println("Age: " + age);
+    }
+
+    public static void doctorDetails() {
+        doctorList = new ArrayList<>();
+
+        doctorList.add(new Doctor("Sathwik", "Cardiologist", "11AM to 3PM", "11", "22"));
+        doctorList.add(new Doctor("Balaji", "Neurologist", "9AM to 12PM", "12", "23"));
+        doctorList.add(new Doctor("Satish", "Kidney Specialist", "10AM to 2PM", "13", "22"));
+        doctorList.add(new Doctor("Lakshman", "Liver Specialist", "4PM to 8PM", "14", "22"));
+        doctorList.add(new Doctor("Pavan", "Psychiatrist", "2PM to 5PM", "15", "21"));
+        doctorList.add(new Doctor("Kalyan", "Radiologist", "5PM to 9PM", "16", "22"));
+        doctorList.add(new Doctor("Dileep", "Neurologist", "3PM to 6PM", "17", "23"));
+        doctorList.add(new Doctor("Tejaswini", "Dermatologist", "10AM to 4PM", "18", "23"));
+        doctorList.add(new Doctor("SaiLakshmi", "Oncologist", "4PM to 9PM", "19", "23"));
+        doctorList.add(new Doctor("Prasanthi", "Neurologist", "6PM to 10PM", "20", "24"));
+
+        for (int i = 0; i < doctorList.size(); i++) {
+            Doctor doctors = doctorList.get(i);
+            doctors.displayDoctorInfo();
+            System.out.println("");
+        }
+
+    }
+    public String getId() {
+        return id;
+    }
+}
---
+++ arraylist/MainClass.java
@@ -0,0 +1,156 @@
+package arraylist;
+import java.util.Scanner;
+import java.util.*;
+import java.util.ArrayList;
+public class MainClass {
+
+	static HashMap<Integer,ArrayList<String>> appointments=new HashMap<>();
+	static Integer appointmentCounter=10;
+
+    public static void main(String[] args) {
+        Doctor.doctorDetails();
+        showOptions();
+    }
+    
+    static Integer getAppointmentId()
+    {
+    	appointmentCounter++;
+    	return appointmentCounter;
+    }
+    
+    static void bookAppointment(String doctorId) {
+    	Scanner sc = new Scanner(System.in);
+    	boolean found = false;
+    	ArrayList<Doctor> doctorsList = Doctor.doctorList;
+    	for (int i = 0; i < doctorsList.size(); i++) {
+            Doctor doctor = doctorsList.get(i);
+            if (doctor.getId().equals(doctorId)) {
+                doctor.displayDoctorInfo();
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Doctor Not Found");
+            return;
+        }
+        
+        System.out.println("Do You Want To Book an Appointment(Yes/No)");
+        String input = sc.next();
+        if (input.equalsIgnoreCase("yes")) {
+
+        	System.out.println("Enter your Name");
+        	String name=sc.next();
+        	System.out.println("Enter your Problem");
+        	String problem=sc.next();
+        	System.out.println("Enter your phone no.");
+        	String phone=sc.next();
+        	
+        	//Appointment details
+        	ArrayList<String> appointmentdetails=new ArrayList<>();
+        	appointmentdetails.add(doctorId);
+        	appointmentdetails.add(name);
+        	appointmentdetails.add(problem);
+        	appointmentdetails.add(phone);
+        	
+        	Integer appointmentID=getAppointmentId();
+        	
+        	appointments.put(appointmentID,appointmentdetails);
+        	
+            System.out.println("Your appointment is booked with Appointment ID: "+appointmentID);
+            
+        } else if (input.equalsIgnoreCase("no")) {
+    
+            System.out.println("Please select another doctor.");
+        } else {
+            System.out.println("Invalid input. Please enter 'yes' or 'no'.");
+        }
+    }
+    
+    static void viewDoctorsInfo() {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Display Doctor Information");
+        Doctor.doctorDetails();
+        
+        System.out.println("Select the Doctor ID Number To Book An Appointment");
+        String selectedDoctorId = sc.next();
+        bookAppointment(selectedDoctorId);
+    }
+    
+    static void displayAppointments()
+    {
+    	for(Integer Y:appointments.keySet())
+    	{
+    		ArrayList<String> appointmentDetails=appointments.get(Y);
+    		System.out.println("Appointment ID:"+Y);
+    		System.out.println("Doctor ID:"+appointmentDetails.get(0));
+    		System.out.println("Patient Name:"+appointmentDetails.get(1));
+    		System.out.println("Patient Problem:"+appointmentDetails.get(2));
+    		System.out.println("Phone:"+appointmentDetails.get(3));
+    		System.out.println();
+    	}
+    }
+    
+    static void deleteAppointment()
+    {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.println("Enter the appointment ID to cancel:");
+    	Integer appointmentId=Integer.valueOf(sc.nextInt());
+    	
+    	if(appointments.containsKey(appointmentId)){
+    		appointments.remove(appointmentId);
+    		System.out.println("Your Appointment was cancelled");
+    	}
+    	else {
+    		System.out.println("appointment details not found.");
+    	}
+    	
+    	
+    }
+
+    public static void showOptions() {
+        Scanner sc = new Scanner(System.in);
+        String input;
+        int options = 0;
+
+        
+        
+        try {
+        	do {
+        	System.out.println("Select any Option");
+            System.out.println("1. Book an Appointment");
+            System.out.println("2. View Doctors Information");
+            System.out.println("3. View Appointments Information");
+            System.out.println("4. Delete an Appointment");
+            System.out.println("5. Exit");
+            options = sc.nextInt();
+            switch (options) {
+                case 1:
+                	System.out.println("Enter the Doctor ID");
+                	String doctorId= sc.next();
+                	bookAppointment(doctorId);
+                    break;
+                case 2:
+                	viewDoctorsInfo();
+                    break;
+                case 3:
+                    displayAppointments();  
+                    break;
+                case 4:
+                    deleteAppointment();
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid Option. Please enter a valid option.");
+                    break;
+            }
+        }while(options != 5);
+    }
+        catch (Exception e) {
+            System.out.println("Please Enter a valid Option");
+        }
+    }
+}
